cd59100d926b651241c3cf142902168ccd53cc1b
Young AOS <106038476+youngaos@users.noreply.github.com>
2023-02-15 21:00:20 -0500

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+import time
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Online"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():  
+    t = Thread(target=run)
+    t.start()
